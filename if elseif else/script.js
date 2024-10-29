91c2a8611cf568abe3deb4a08f7b54951fa4c5c7
vusalasalimovaa <salimovavusala7@gmail.com>
2024-10-29 18:07:48 +0400

    js

---
+++ if elseif else/script.js
@@ -0,0 +1,157 @@
+// let num= +prompt("yasinizi yazin")
+// if (num) {
+//   num>=64;
+//   console.log("penisiyaya dusursuz");
+
+// }else{
+// console.log("pensiyaya dusmursuz");
+
+// let a = +prompt("terefi daxil edin");
+// let b = +prompt("terefi daxil edin");
+// let c = +prompt("terefi daxil edin");
+
+// if (a === b && a === c) {
+//   console.log("ucbucaq beraberterefli");
+// } else if (a === b && a !== c) {
+
+//   console.log("ucbucaq beraberyanli");
+// } else if (a !== b && a === c) {
+
+//   console.log("ucbucaq muxtelif");
+// }
+// else{
+
+// }
+// let a = +prompt("aededini daxil edin");
+
+// if (a%2===0) {
+//   console.log("eded cutdur");
+
+// } else if($a%2!==0) {
+//   console.log("eded cutdur");
+
+// }
+// else{
+//   console.log("eded 0dir");
+
+// }
+// let ay = +prompt("ayin nomresini tapin");
+// switch (ay) {
+//   case 12:
+
+//   case 1:
+
+//   case 2:
+//     alert("qis");
+//     break;
+//   case 3:
+
+//   case 4:
+
+//   case 5:
+//     alert("yaz");
+//     break;
+//   case 6:
+
+//   case 7:
+
+//   case 8:
+//     alert("yay");
+//     break;
+//   case 9:
+
+//   case 10:
+
+//   case 11:
+//     alert("payiz");
+//     break;
+//   default:
+//     ay>12;
+//     console.log("bele bir fesil yoxsdur");
+
+//     break;
+// }
+// Daxil edilən müsbət ədədin 3 və 5 ədədinə eyni anda bölünüb-b ölünmədiyini tapan proqram yazın .
+// let a=+prompt("a ededini daxil edin");
+// if (a%3==0 && a%5==0) {
+//   console.log(`${a} ededi 3e ve 5e qaliqsiz bolunur`);
+
+// }else{
+//   console.log(`${a} ededi 3e ve 5e qaliqsiz bolunmur`)
+// }
+// Task 6:
+
+// Daxil edilən müsbət ədədin 2-yə tam bölünüb-bölünmədiyini tapan proqram yazın. Əgər tam bölünürsə, consolda bölündü yazılsın, tam bölünmürsə qalıq hesablanıb yazılsın.
+// let a =+prompt("a edediniu daxil edin");
+// if (a%2==0) {
+//   console.log(`${a} ededi bolunur`)
+// }else {
+//   console.log(`${a} ededi bolunmur`)
+// }
+// Task 8:
+
+// Daxil edilən müsbət ədədin 1-100 arasında olub-olmadığını tapan proqram yazın.
+// let a = +prompt ("a ededini daxil edin");
+// if (a>=1 && a<= 100) {
+//   console.log("dogrudur");
+
+// }else{
+//   console.log("dogru degil");
+
+// }
+// Task 9:
+
+// Daxil edilən 3 müsbət ədədin ən böyüyünü tapan proqram yazın.
+// let a = +prompt("a ededini daxil edin");
+// let b = +prompt("b ededini daxil edin");
+// let c = +prompt("c ededini daxil edin");
+// if (a>b>c) {
+//   console.log(`${a} boyukdur`);
+// }else if(b>a>c){
+//   console.log(`${b} boyukdur`);
+// }
+// else if(c>b>a){
+//   console.log(`${c} boyukdur`);
+// }else{
+//   console.log("eded menfidir");
+// }
+// Tələbənin daxil etdiyi bala görə ABCDEF qiymətlərindən hansını aldığını müəyyənləşdirrən proqram yazın. 90 və yuxarı üçün "A", 80-89 üçün "B", 70-79 üçün "C", 60-69 üçün "D", 59 və daha aşağı qiymətlər üçün "F". Daxil edilən rəqəm müsbət, sıfırdan böyük və 100-dən kiçik olmalıdır.
+
+//
+// let a = +prompt("a ededini daxil edin:");
+// let b = +prompt("b ededini daxil edin:");
+// let c = +prompt("c ededini daxil edin:");
+
+// let sum = a + b + c;
+// let average = sum / 3;
+// alert(`${(a+b+c)/3}`);
+// let a = prompt('adinizi daxil edin');
+// let b = prompt('soyadinizi daxil edin');
+// let fullname= a+ b;
+// alert(fullname);
+let emekhaqqi = prompt("emek haqqinizi daxil edin");
+let istediyinizmeblegh =prompt("istediyiniz mebleghi daxil edin");
+let istediyinizmuddet=prompt("istediyiniz muddet (ay)");
+if (istediyinizmuddet>=36) {
+  alert("size kredit dusmur");
+
+} else{
+  if (istediyinizmeblegh>=emekhaqqi*10) {
+    alert("size kredit dusmur");
+  }
+  else{
+    if(istediyinizmuddet>0 && istediyinizmuddet<=12){
+      mebleg+=(mebleg*13) / 100
+      if (condition) {
+        
+      }
+    }
+    else if (istediyinizmuddet>12 && istediyinizmuddet<=24){
+
+    }
+    else if (istediyinizmuddet>24 && istediyinizmuddet<=36){
+
+    }
+
+  }
+}
