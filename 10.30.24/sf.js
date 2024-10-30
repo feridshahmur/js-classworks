be1d2d8a32d180ad5b8bd58af7bd093708691e29
vusalasalimovaa <salimovavusala7@gmail.com>
2024-10-30 19:25:25 +0400

    loops

---
+++ 10.30.24/sf.js
@@ -0,0 +1,172 @@
+// let sum;
+// for (let index = 0; index <= 100; index++) {
+//   if (index % 2 == 0) {
+//     console.log(index);
+
+//   }
+
+// }
+// let count = 0;
+// for (let i = 0; i <= 100; i++) {
+//   if (i%2==1) {
+//     count=i++;
+
+//   }
+
+// }
+// console.log(count);
+// let sum = 0;
+// for (let i = 0; i <=100; i++) {
+//   if (i%5==0) {
+//     sum+=i
+//   }
+
+// }
+// console.log(sum);
+// let = array[0];
+// for (let i = 0; i < 15; i++) {
+//  if (i%2!=0) {
+//   `console.log`
+//  }
+
+// }
+
+// let sum = 0;
+
+// for (let i = 0; i < arr.length; i++) {
+//   sum+=arr[i]
+
+// }
+// console.log(sum);
+// #arrayın tək index-də duran elementlərin tapın.
+// let sum = 0
+// let ar=[];
+// let array =[12,14,35,56,68,93];
+// for (let i = 0; i < array.length; i++) {
+//   if(i%2==1){
+//     console.log(array[i]);
+
+//   }
+// }
+// let sum = 0
+
+// let array =[12,14,35,56,68,93];
+// for (let i = 0; i < array.length; i++) {
+//   if(i%2==0){
+
+//     sum+=array[i]
+
+//   }
+// }
+// console.log(sum);
+// #arrayin daxilindəki  ən böyük   elementi return edən funksiya yazın
+
+// let arr = [ 12, 14, 35, 56, 68, 93];
+// let largest = arr[0];
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] > largest) {
+//     largest = arr[i];
+//   }
+// }
+// console.log(largest);
+// let arr = [ 12, 14, 35, 56, 68, 93];
+// let largest = arr[0];
+
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] > largest) {
+//     largest = arr[i];
+//   }
+// }
+// // console.log(largest);
+// let smallest = arr[0]
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] > largest) {
+//     largest = arr[i];
+//   }
+// }
+// // console.log(smallest);
+// let ferq = largest - smallest;
+// console.log(ferq);
+// #verilmiş ədədin (example:25)array'in elementləri arasında olub olmadığını müəyyən edən proqram yazn
+// 
+// filterEmployees adında funksiya yaradın, parameter kimi bir array alsın və salary > 60000 olan objectləri return etsin
+// const employees = [
+//   { name: "Jamil", salary: 50000, department: "IT" },
+//   { name: "Jale", salary: 60000, department: "HR" },
+//   { name: "Bayram", salary: 55000, department: "IT" },
+//   { name: "Sahil", salary: 75000, department: "HR" },
+//   { name: "Maryam", salary: 65000, department: "IT" },
+//   { name: "Elnara", salary: 80000, department: "HR" },
+//   { name: "Davud", salary: 70000, department: "IT" },
+// ];
+
+// for (let index = 0; index < employees.length; index++) {
+//   if (employees[index].salary>60000) {
+//     console.log(employees[index].name);
+    
+//   }
+  
+// }
+// let a = 5;
+// let faktorial=1;
+// for (let i = 1; i>a; i++) {
+  
+//   faktorial*=i;
+//   console.log(faktorial);
+// }
+// let count = 0;
+// let str = "lorrem ipsum dolor"
+// for (let i = 0; i < str.length; i++) {
+//   if (str[i]=="l") {
+//     count++
+//   }
+  
+// }
+// console.log(count);
+// const countries = ["azerbaijan", "america","colombia"]
+// for (let i = 0; i < countries.length; i++) {
+//   if (countries[i][0] && countries [countries.length-1] == "a") {
+//     console.log(countries[i]);
+    
+//   }
+  
+// }
+// let n = 427;
+// let first;
+// let second;
+// let third;
+// while (n>0) {
+  
+// }
+// let a = 17;
+// let murekkeb = true;
+// for (let i = 2; i < a; i++) {
+//   if ( a%i==0) {
+//    murekkeb=true
+//     break;
+    
+//   }
+// } 
+// if (murekkeb == true) {
+//   console.log("murekkeb");
+  
+// } else {
+//   console.log("murekkeb deyil");
+  
+// }
+// const scores = [60 , 75 , 88 , 75 , 93 , 45]
+// let sum =0 ;
+// for (let i = 0; i < scores.length; i++) {
+//  sum+= scores[i];
+//  sum=sum/scores.length;
+// }
+// console.log(sum);
+const scores = [60 , 75 , 88 , 75 , 93 , 45]
+let min = scores[0];
+for (let i = 0; i < scores.length; i++) {
+  if (scores[i]< min) {
+    
+    
+  }
+}
+console.log(min);
